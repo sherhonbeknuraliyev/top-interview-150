b3f54b0a62d476135617ea50e155648f0ff82aef
diyora <diyoramekhmonova@gmail.com>
2024-08-01 10:21:09 +0800

    feat: remove dp from sorted array 2

---
+++ ts/src/remove_dp_from_sorted_array2.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@jest/globals";
+
+import removeDpFromSorted2 from "./remove_dp_from_sorted_array2";
+
+test("remove duplicates from sorted array", () => {
+  expect(removeDpFromSorted2([1, 1, 1, 2, 2, 3])).toBe(5);
+  expect(removeDpFromSorted2([0, 0, 1, 1, 1, 1, 2, 3, 3])).toBe(7);
+});
